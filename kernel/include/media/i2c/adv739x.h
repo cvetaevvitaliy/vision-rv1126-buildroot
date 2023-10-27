86a349ce609dfdb2cc6c452ed464b18cdb023e4a
nacuk <vitaliy.nimych@gmail.com>
2023-10-27 16:18:24 +0300

    add adv739x driver

---
+++ kernel/include/media/i2c/adv739x.h
@@ -0,0 +1,21 @@
+/*
+ * ADV739X header file
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed .as is. WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef ADV739X_H
+#define ADV739X_H
+
+#define ADV739X_COMPOSITE_ID	(0)
+#define ADV739X_COMPONENT_ID	(1)
+#define ADV739X_SVIDEO_ID	(2)
+
+#endif  /* End of #ifndef ADV739X_H */
